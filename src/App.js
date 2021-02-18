317afe4b343f25d784c008a52919d976f4d755a1
OlgaMicony <73658103+OlgaMicony@users.noreply.github.com>
2021-02-18 13:05:14 +0100

    Added card

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">   
-    <div classNemw="conteiner">   
-      <Weather />   
+    <div classNemw="conteiner"> 
+    <div class="card">
+      <div class="card-body">
+        <Weather />
+      </div>
+          <footer className="link">
+            <a href="https://github.com/OlgaMicony/weather-app-react" target="_blank" rel="noreferrer">Open-source code </a>by Olga Micony 
+          </footer>  
+           
+   </div>  
+      
       </div>  
     </div>
   );
